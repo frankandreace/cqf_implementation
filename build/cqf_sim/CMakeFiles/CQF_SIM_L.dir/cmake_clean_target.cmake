f31ab365e016b616a91ffa17be162a4f71b0f0cf
Yoann Dufresne <yoann.dufresne0@gmail.com>
2022-12-30 14:15:23 +0100

    cleaning the repo

--- build/cqf_sim/CMakeFiles/CQF_SIM_L.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libCQF_SIM_L.a"
-)
--- build/cqf_sim/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/francesco/cqf_test-master/cqf_sim
-# Build directory: /home/francesco/cqf_test-master/build/cqf_sim
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
