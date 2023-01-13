4c2802bd5a9f896b10bf7926c11c895d480b17ee
Yoann Dufresne <yoann.dufresne0@gmail.com>
2023-01-13 11:51:12 +0100

    add debug flag to test presence/absence

--- examples/random_uints.cpp
+++ examples/random_uints.cpp
@@ -7,37 +7,64 @@
 #include <random>
 #include <ctime>
 #include <getopt.h>
+#include <unordered_set>
 #include "filter.hpp"
 
 
 using namespace std;
 
 
-void parse_cmd(int argc, char *argv[], size_t & n, uint64_t & seed, bool & verbose);
+void parse_cmd(int argc, char *argv[], size_t & n, size_t & filter_size, uint64_t & seed, bool & verbose, bool & debug);
 
 
 int main (int argc, char * argv[]) {
 	// Argument prep and parsing
 	size_t n = 0;
+	size_t filter_size = 4;
 	uint64_t seed = time(NULL);
 	bool verbose = false;
-	parse_cmd(argc, argv, n, seed, verbose);
+	bool debug = false;
+	parse_cmd(argc, argv, n, filter_size, seed, verbose, debug);
 
 	// Creation of the cqf (size is MB)
-	uint64_t quotien_size = 1;
-	Cqf cqf(quotien_size);
+	Cqf cqf(filter_size, verbose);
 
 	// uint64 generators
 	default_random_engine generator;
 	generator.seed(seed);
 	uniform_int_distribution<uint64_t> distribution;
 
+	// Debug
+	unordered_set<uint64_t> verif;
+
 	// Add the uints one by one into the cqf
 	for (size_t i=0 ; i<n ; i++) {
 		uint64_t val = distribution(generator);
-		cout << i << " " << val << endl;
+		if (debug)
+			verif.insert(val);
 		cqf.insert(val);
-		cout << endl;
+	}
+
+	// Verification
+	if (debug) {
+		// Query present
+		uint64_t total_present = 0;
+		cout << "Querying present values" << endl;
+		for (const uint64_t val : verif) {
+			uint64_t count = cqf.query(val);
+			total_present += count;
+		}
+		cout << total_present << "/" << n << " are correctly present" << endl;
+
+		// Query absent
+		uint64_t total_absent = 0;
+		cout << "Querying absent values" << endl;
+		for (size_t i=0 ; i<n ; i++) {
+			uint64_t val = distribution(generator);
+			uint64_t count = cqf.query(val);
+			total_absent += count == 0 ? 1 : 0;
+		}
+		cout << total_absent << "/" << n << " are correctly absent" << endl;
 	}
 
 	return 0;
@@ -45,17 +72,19 @@ int main (int argc, char * argv[]) {
 
 
 void print_cmd() {
-	cerr << "cmd line: random_uints -n <value> [-s <rnd_seed>] [-v]" << endl;
+	cerr << "cmd line: random_uints -n <value> [-s <size>] [-r <rnd_seed>] [-v] [-d]" << endl;
 	cerr << "   -n <val>: Number of random uint to insert." << endl;
-	cerr << "   -s <val>: uint64_t random seed." << endl;
+	cerr << "   -s <val>: Filter size (in MB) [default: 4]." << endl;
+	cerr << "   -r <val>: uint64_t random seed." << endl;
+	cerr << "   -d: debug flag." << endl;
 	cerr << "   -v: verbose flag." << endl;
 }
 
 
-void parse_cmd(int argc, char *argv[], size_t & n, uint64_t & seed, bool & verbose) {
+void parse_cmd(int argc, char *argv[], size_t & n, size_t & filter_size, uint64_t & seed, bool & verbose, bool & debug) {
 	int opt;
 
-    while ((opt = getopt(argc, argv, "hn:s:v")) != -1) {
+    while ((opt = getopt(argc, argv, "hn:s:vd")) != -1) {
         switch (opt) {
         	case 'h':
         		print_cmd();
@@ -63,13 +92,19 @@ void parse_cmd(int argc, char *argv[], size_t & n, uint64_t & seed, bool & verbo
             case 'n':
                 n = atoi(optarg);
                 break;
+            case 'r': {
+	    		istringstream iss(optarg);
+                iss >> seed; }
+                break;
             case 's': {
 	    		istringstream iss(optarg);
-                iss >> seed;
-            	}
+                iss >> filter_size; }
                 break;
             case 'v':
                 verbose = true;
+                break;
+            case 'd':
+                debug = true;
                 break;
             default:
             	print_cmd();
